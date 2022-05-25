177cabdf09fb8da05b85d8a52d2edc171c8a4052
Pablo <pablo.developer0@gmail.com>
2022-05-25 18:44:49 +0200

    create react app + cleaning files

---
+++ ecommerce/src/App.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <section className='App-content'>ecommerce</section>
+    </div>
+  )
+}
+
+export default App
